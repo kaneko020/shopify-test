4eeadb9e5fde866fceaec23f865c8f3de19c6b4b
kaneko020 <114220837+kaneko020@users.noreply.github.com>
2024-11-13 12:43:54 +0900

    Update checkout-modal.js

--- checkout-modal.js
+++ checkout-modal.js
@@ -1,42 +1,12 @@
 console.log('checkout-modal.js loaded');
 
-const modalHtml = `
-<div id="myModal" style="display:none;">
-    <div class="modal-content">
-        <span class="close">&times;</span>
-        <h2>ご確認ください</h2>
-        <p>この内容でお間違いありませんか？</p>
-    </div>
-</div>
-`;
-
-document.body.innerHTML += modalHtml;
-
 document.addEventListener('DOMContentLoaded', function() {
     // チェックアウトボタンの要素を監視する
-    const payButton = document.querySelector('.checkout');
+    const payButton = document.querySelector('.checkout-pay-button');
 
     if (payButton) {
-        console.log('checkout');
         payButton.addEventListener('click', function(event) {
-            event.preventDefault(); // デフォルトのアクションをキャンセル
-
-            // モーダルを表示
-            const modal = document.getElementById('myModal');
-            modal.style.display = "block";
-
-            // 閉じるボタンの処理
-            const closeButton = modal.querySelector('.close');
-            closeButton.onclick = function() {
-                modal.style.display = "none";
-            }
-
-            // モーダルの外側をクリックしたときにモーダルを閉じる処理
-            window.onclick = function(event) {
-                if (event.target == modal) {
-                    modal.style.display = "none";
-                }
-            }
+            console.log('checkout click');
         });
     }
 });
